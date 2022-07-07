a1f8e634a257503bc77fc777488378b1ca8beba8
Cleillton-Silva <scleiilton@gmail.com>
2022-07-07 10:00:10 -0300

    criação do cadrastro do evento

--- evento.js
+++ evento.js
@@ -1,21 +1,38 @@
-}
-else  if  ( dataAtual  >  dataEvento )  {
-consola . log ( "Cadastro não permitido por dados inválidos" )
-processo . saída ( )
-}
+// Empresa Savanis \\
 
-let  idade  =  parseInt ( prompt ( "Digite sua idade: " ) )
-@@ -22,6 +23,7 @@ if (idade >= 18) {
+let  prompt  =  require ( 'prompt-sync' )  ( )
+
+
+dataAtual  =  nova  data
+consola . log ( "Hoje é "  +  dataAtual . getDate ( )  +  "-"  +  dataAtual . getMonth ( )  +  "-"  +  dataAtual . getFullYear ( )  )  //Obtendo o dia, mês e ano atual do usuário
+
+let  dataEvento  =  new  Date ( "2022-08-28" )  // Salvando a data do evento com uma string
+
+if  ( dadosAtual  <  dadosEvento )  {
+    consola . log ( "Continuação de cadastro..." )
 }
-senão  se  ( idade  <  18 )  {
-    consola . log ( "Cadastro não permitido. Idade mínima menor que a permitido." )
+    else  if  ( dataAtual  >  dataEvento )  {
+    consola . log ( "Cadastro não permitido por dados inválidos" )
     processo . saída ( )
 }
+ 
+let  idade  =  parseInt ( prompt ( "Digite sua idade: " ) )
+
+if  ( idade  >=  18 )  {
+    consola . log ( "Continuação de cadastro..." )
+}
+    senão  se  ( idade  <  18 )  {
+        consola . log ( "Cadastro não permitido. Idade mínima menor que a permitido." )
+        processo . saída ( )
+    }
 
 deixe  listaParticipantes  =  [ 98 ] ;
-@@ -32,5 +34,6 @@ if (listaParticipantes <=100) {
+
+consola . log ( `Número de participantes: ${ listaParticipantes [ 0 ] } ` ) ;
+if  ( listaParticipantes  <= 100 )  {
+    consola . log ( "Cadastro concluído!" )
 }
 else  if  ( listaParticipantes  > 100 )  {
-consola . log ( "Cadastro não concluído. Número de participantes por evento excedido." )
-processo . saída ( )
+    consola . log ( "Cadastro não concluído. Número de participantes por evento excedido." )
+    processo . saída ( )
 }
